596a2c9df6b81e569bd48a4304e44b0753be9707
Angel Gabriel <aghh.oficial@gmail.com>
2026-04-02 22:50:36 -0600

    Added config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      load: [ EnvConfiguration ],
+      // isGlobal: true
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..','public')
     }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
+      imports:[ConfigModule],
       useFactory: async(configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB')
       }),
